a89b5b08228f28541bf6876596ae315a07a2793d
Glory <u8L1.M6h1.Q3p7.J9a2@gmail.com>
2024-09-13 11:12:09 +0300

    Done?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,26 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import netlify from '@astrojs/netlify';
 
-// https://astro.build/config
-export default defineConfig({});
+
+import mdx from '@astrojs/mdx';
+
+export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
+  integrations: [
+    mdx({
+      // MathJax
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+      // MDX
+      extendPlugins: 'defaults',
+      extendMarkdownConfig: false,
+      smartypants: true,
+      gfm: true,
+    }),
+  ],
+});
+
